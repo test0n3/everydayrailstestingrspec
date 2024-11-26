e7a03cb72ab90f8d12c3758a587f969df2e30093
Carlos A <cayala.w@gmail.com>
2024-11-25 21:00:06 -0500

    testing models and instance methods

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    project = user.projects.build(name: "Test Project")
+    expect(project).to be_valid
+    # project.valid?
+    # expect(project).to_not be_valid
+  end
+
+  it "is invalid without a name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    project = user.projects.build(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+    # expect(project).to be_valid
+  end
+
+  it "does not allow duplicate project names per user" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project",
+    )
+
+    new_project = user.projects.build(
+      name: "Test Project",
+    )
+
+    new_project.valid?
+    expect(new_project.errors[:name]).to include("has already been taken")
+    # expect(new_project).to be_valid
+  end
+
+  it "allows two users to share a project name" do
+    user = User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project",
+    )
+
+    other_user = User.create(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "janetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    other_project = other_user.projects.build(
+      name: "Test Project",
+    )
+
+    expect(other_project).to be_valid
+    # other_project.valid?
+    # expect(other_project.errors[:name]).to include("has already been taken")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name, email, and password'
-  it 'is invalid without a first name'
-  it 'is invalid without a last name'
-  it 'is invalid without an email address'
-  it 'is invalid with a duplicate email address'
-  it 'returns a user\'s full name as a string'
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavillion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a first name" do
+    user = User.new(first_name: nil)
+    user.valid?
+    # expect(user.errors.size).to eq 1
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+  
+  it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "Joe",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user = User.new(
+      first_name: "Jane",
+      last_name: "Tester",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user.name).to eq "Aaron Sumner"
+  end
 end
